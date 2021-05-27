f7cf64677c27c5cbeb71e3e54e8bbaca57773c54
XiaoWei <xwei44@student.monash.edu>
2021-05-27 16:20:28 +1000

    updated expandable list view

--- app/src/main/java/com/example/sit708_assessment3/TaskDetails.java
+++ app/src/main/java/com/example/sit708_assessment3/TaskDetails.java
@@ -1,17 +1,30 @@
 package com.example.sit708_assessment3;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Objects;
 
 public class TaskDetails extends AppCompatActivity {
-    TextView textTitle;
+    ExpandableListView details;
+    List<String> detailCategory;
+    HashMap<String, String> detailInfo;
+    DetailAdapter adapter;
+    int index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +32,32 @@ public class TaskDetails extends AppCompatActivity {
         setContentView(R.layout.tasks_details);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        Intent i = getIntent();
-        String title = i.getStringExtra("title") + " Content";
-        textTitle = findViewById(R.id.textView);
-        textTitle.setText(title);
-        this.setTitle(i.getStringExtra("title"));
+        details = findViewById(R.id.details);
+        detailCategory = new ArrayList<>();
+        detailInfo = new HashMap<>();
+        adapter = new DetailAdapter(this, detailCategory, detailInfo);
+        details.setAdapter(adapter);
 
+        Bundle extras = getIntent().getExtras();
+        String task_name = extras.getString("title");
+        index = extras.getInt("index");
+        this.setTitle(task_name);
+        initListData();
+    }
+
+    private void initListData() {
+        detailCategory.add(getString(R.string.category_what));
+        detailCategory.add(getString(R.string.category_why));
+        detailCategory.add(getString(R.string.category_how));
+
+        String[] info_what = getResources().getStringArray(R.array.category_what);
+        String[] info_why = getResources().getStringArray(R.array.category_why);
+        String[] info_how = getResources().getStringArray(R.array.category_how);
+
+        detailInfo.put(detailCategory.get(0), info_what[index]);
+        detailInfo.put(detailCategory.get(1), info_why[index]);
+        detailInfo.put(detailCategory.get(2), info_how[index]);
+        adapter.notifyDataSetChanged();
     }
 
     @Override
